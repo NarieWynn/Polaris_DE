c931e59650ec34ec62b67c93c0ad53ec52771980
NarieWynn <huynhngocnguyen24@gmail.com>
2026-06-18 10:34:52 +0700

    Finding app on launcher

--- src/main.cpp
+++ src/main.cpp
@@ -8,17 +8,6 @@ using namespace Qt::StringLiterals;
 int main(int argc, char *argv[]) {
     QGuiApplication app(argc, argv);
 
-    qDebug() << "--- DANH SÁCH FILE QML ĐANG CÓ TRONG HỆ THỐNG QRC: ---";
-    QDirIterator it(":", QDirIterator::Subdirectories);
-    while (it.hasNext()) {
-        QString path = it.next();
-        if (path.endsWith(".qml")) {
-            qDebug() << "-> Tìm thấy file QML tại:" << path;
-        }
-    }
-    qDebug() << "----------------------------------------------------";
-
-
     QQmlApplicationEngine engine;
 
     const QUrl url(u"qrc:/polaris/qml/main.qml"_s);
